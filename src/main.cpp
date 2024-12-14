40ac8d6f27ca8e8bc7c4e126a27783f5f20bda28
ProCrafter67 <procrafter067@gmail.com>
2024-12-14 18:13:18 +0530

    Small changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include <raylib.h>
 
-int main (int argc, char** argv) {
-  std::cout << "Hello World!" << std::endl;
+int main (int argc, char** argv)
+{
+  Color green = {47, 109, 189, 255};
 
   InitWindow(800, 600, "PPGame");
+  SetTargetFPS(60);
 
   while (!WindowShouldClose())
   {
     BeginDrawing();
-      ClearBackground(RAYWHITE);
+    ClearBackground(green);
     EndDrawing();
   }
 
