42365c3d6b0e48c980ed239942729e6cdba7dfbb
CloudCodingSpace <subhamaysaha11@gmail.com>
2024-12-14 18:32:15 +0530

    Added ball movements

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,53 @@
-#include <iostream>
 #include <raylib.h>
 
-int main (int argc, char** argv)
-{
+struct Circle {
+  float radius;
+  Vector2 pos;
+  Color color;
+};
+
+#define WIDTH  800
+#define HEIGHT 600
+
+#define BALL_SPEED 5
+
+int main(int argc, char** argv) {
+  InitWindow(WIDTH, HEIGHT, "PPGame");
+
   Color green = {47, 109, 189, 255};
+  Circle ball{};
+  ball.radius = 30;
+  ball.pos = (Vector2) { (int)(WIDTH/2), (int)(HEIGHT/2) };
+  ball.color = {255, 0, 0, 255};
 
-  InitWindow(800, 600, "PPGame");
   SetTargetFPS(60);
-
   while (!WindowShouldClose())
   {
     BeginDrawing();
     ClearBackground(green);
+    
+    // Rendering stuff
+    {
+      DrawCircle(ball.pos.x, ball.pos.y, ball.radius, ball.color);
+    } 
+
     EndDrawing();
+
+    // Update
+    {
+      if(IsKeyPressed(KEY_W)) {
+        ball.pos.y -= BALL_SPEED;
+      }
+      if(IsKeyPressed(KEY_S)) {
+        ball.pos.y += BALL_SPEED;
+      }
+      if(IsKeyPressed(KEY_A)) {
+        ball.pos.x -= BALL_SPEED;
+      }
+      if(IsKeyPressed(KEY_D)) {
+        ball.pos.x += BALL_SPEED;
+      }
+    }
   }
 
   CloseWindow();
